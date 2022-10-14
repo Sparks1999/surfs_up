c51b173004278ea3e5cc2b994fbdc3131d607211
acct.1 <acct.1@acct1s-MacBook-Pro.local>
2022-10-13 21:29:03 -0700

    Fifth push

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 #creating app routes
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return '''Hello World
+    Hello World'''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
